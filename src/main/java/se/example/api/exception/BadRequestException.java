6d85adf8b1873d32c3026ae38c873bfbc04d3fbc
sumigg <with.sumitaggarwal@gmail.com>
2025-08-12 10:27:14 +0530

    add changes for chapter5 openapi swaggerui

---
+++ src/main/java/se/example/api/exception/BadRequestException.java
@@ -0,0 +1,21 @@
+package se.example.api.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException() {
+        super("Bad request");
+    }
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadRequestException(Throwable cause) {
+        super(cause);
+    }
+
+}
