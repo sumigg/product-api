ac990552b5d526f83d3ab834994a080610d7bb26
sumigg <with.sumitaggarwal@gmail.com>
2025-09-17 07:51:56 +0000

    add chapter11 security changes

--- src/main/java/se/example/api/composite/product/ProductCompositeService.java
+++ src/main/java/se/example/api/composite/product/ProductCompositeService.java
@@ -3,11 +3,13 @@ package se.example.api.composite.product;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import reactor.core.publisher.Mono;
 
 import org.springframework.web.bind.annotation.*;
 
+@SecurityRequirement(name = "security_auth")
 @Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
